ca088b598c5bfbb39973fbe2a550829489694e17
nuchnachaphonchoto <song290144@gmail.com>
2025-05-01 13:38:27 +0700

    implement zklogin

--- frontend/src/providers.tsx
+++ frontend/src/providers.tsx
@@ -1,8 +1,8 @@
-import { WalletKitProvider } from '@mysten/wallet-kit';
-import { WagmiProvider, createConfig, http } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
-import { injected } from '@wagmi/connectors';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WalletKitProvider } from '@mysten/wallet-kit'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { mainnet } from 'wagmi/chains'
+import { injected } from '@wagmi/connectors'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const wagmiConfig = createConfig({
   chains: [mainnet],
@@ -10,18 +10,18 @@ const wagmiConfig = createConfig({
   transports: {
     [mainnet.id]: http(),
   },
-});
+})
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <WalletKitProvider>
         <WagmiProvider config={wagmiConfig}>
-          {children}
+            {children}
         </WagmiProvider>
       </WalletKitProvider>
     </QueryClientProvider>
-  );
+  )
 }
